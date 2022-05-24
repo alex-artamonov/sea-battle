25ca6a9434473f9234bae8e7d870181be24a64e6
alex-artamonov <alex.artamonov2010@gmail.com>
2022-05-24 20:30:16 +0300

    gameplay started

--- gameplay.py
+++ gameplay.py
@@ -1,30 +1,49 @@
 # -*- coding: utf-8 -*-
-from classes import Ship, Board
+from ship import Ship
+from board import Board
 from sys import exit
+from itertools import zip_longest
+
 INP_INVITE = "?--> "
 
 sh = Ship(4, "H", (1, 1))
+
+
 # print(sh)
 
 def to_lines_by_limit(string, limit):
     """Разбивает сплошной текст на строки с указанным
     лимитом знаков, не разбивая конечные слова"""
+    # for word in string.split():
+    #     if len(word) > limit:
+    #         raise ValueError(f"The word '{word}' is too long for the specified limit '{limit}'")
     output = ""
     while len(string) >= limit:
         temp = string[:limit]
         string = string[limit:]
         n = temp.rfind(' ')
-        head = temp[:n].lstrip()
-        tail = temp[n:]
-        string = tail + string
+        if n > 0:
+            # print("temp:", temp)
+            # raise ValueError(f"the limit is too short for the word '{temp}' and limit '{limit}'")
+
+            head = temp[:n].lstrip()
+            tail = temp[n:]
+            string = tail + string
+
+        elif n <= 0:
+            raise ValueError(f"The limit '{limit}' is too small: the text can't be split")
+        else:  # если пробел в начале строки
+            head = temp.lstrip()
+            print("from else:", head)
         output += head + "\n"
         # print(head, "\t", len(head))
     # print(s.lstrip())
     output += string.lstrip()
     return output
 
-def print_side_by_side(str1, str2, divider = "    |    "):
-    """выводит попеременно строки из двух строковых переменных
+
+def print_side_by_side(str1, str2, divider="    |    "):
+    """Выводит попеременно строки из двух строковых переменных
     с указанным разделением между ними"""
     s1 = str1.split('\n')
     s2 = str2.split('\n')
@@ -32,9 +51,10 @@ def print_side_by_side(str1, str2, divider = "    |    "):
     lst = [len(elem) for elem in s1]
     nbr_spaced = max(lst)
 
-
-    for line1, line2 in zip(s1, s2):
+    for line1, line2 in zip_longest(s1, s2):
+        line1, line2 = line1 or "", line2 or ""
         print(line1.ljust(nbr_spaced) + divider + line2)
+
 
 s1 = "Some people take the guideline “the shorter, the better” to an extreme and\
 claim that all functions should be three or four lines of code at most. This is\
@@ -51,6 +71,7 @@ s2 = "Согласно пункту 2 Порядка выдачи разреше
 (при наличии), а также должно содержать дату и регистрационный (исходящий)\
 номер. Дополнительных требований к доверенности не предусмотрено."
 
+
 # s1, s2 = display_board("human:\n".center(25)), display_board("computer:\n".center(25))
 
 
@@ -60,21 +81,20 @@ def greeting():
     print("Привет,", name)
 
 
-
 def get_player_move():
-
     while True:
         response = input(INP_INVITE).upper().strip()
         if response == "QUIT":
             print("Спасибо за игру!")
             exit()
 
+
 # greeting()
 # getPlayerMove()
 
 # print(to_lines_by_limit(s1, 20))
-s1 = to_lines_by_limit(s1, 15)
-print(s1)
+
+# print(to_lines_by_limit(s1, 26))
 # s2 = to_lines_by_limit(s2, 25)
 # print(s2)
-# print_side_by_side(s1, s2)
+print_side_by_side(to_lines_by_limit(s1, 26), to_lines_by_limit(s2, 26))
